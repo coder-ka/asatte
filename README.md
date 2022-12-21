b5a4ececb25124483fc18b513ff80aba50c3ce02
Katsuyuki Oeda <katsu.amagramer@gmail.com>
2022-12-22 01:23:07 +0900

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,57 @@
+# asatte
+
+Asatte is immutable date manipulation library for minimalist.
+
+There is no syntax (e.g. "d" "y").
+
+## Motivation
+
+I love [Fecha](https://github.com/taylorhakes/fecha).
+
+Fecha is the best date library ever for me but no manipulation provided.
+
+Yes, you can do that with JavaScript `Date` Object.
+
+```js
+const today = new Date();
+const dayAfterTomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 2, today.getHours(), today.getMinutes(), today.getSeconds(), today.getMilliseconds());
+```
+
+With asatte
+
+```js
+import { addD } from "asatte";
+
+const today = new Date();
+const dayAfterTomorrow = addD(2, today);
+```
+
+Which do you like?
+
+# Installation
+
+```bash
+npm i asatte
+```
+
+# Functions
+
+You can understand what function to use with these signatures.
+
+- addY(years: number, date: Date): Date
+- addMo(months: number, date: Date): Date
+- addD(dates: number, date: Date): Date
+- addH(hours: number, date: Date): Date
+- addMi(minutes: number, date: Date): Date
+- addS(seconds: number, date: Date): Date
+- addMS(milliseconds: number, date: Date): Date
+
+- setY(years: number, date: Date): Date
+- setMo(months: number, date: Date): Date
+- setD(dates: number, date: Date): Date
+- setH(hours: number, date: Date): Date
+- setMi(minutes: number, date: Date): Date
+- setS(seconds: number, date: Date): Date
+- setMS(milliseconds: number, date: Date): Date
+
+And every function is curried!
--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,157 @@
-export function greeting() {
-  return "Hello World."
-}
+import curry from "just-curry-it";
+
+export const addY = curry((years: number, date: Date) => {
+  return new Date(
+    date.getFullYear() + years,
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const addMo = curry((months: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth() + months,
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const addD = curry((dates: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate() + dates,
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const addH = curry((hours: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours() + hours,
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const addMi = curry((minutes: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes() + minutes,
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const addS = curry((seconds: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds() + seconds,
+    date.getMilliseconds()
+  );
+});
+export const addMS = curry((milliseconds: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds() + milliseconds
+  );
+});
+
+export const setY = curry((years: number, date: Date) => {
+  return new Date(
+    years,
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const setMo = curry((months: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    months,
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const setD = curry((dates: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    dates,
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const setH = curry((hours: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    hours,
+    date.getMinutes(),
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const setMi = curry((minutes: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    minutes,
+    date.getSeconds(),
+    date.getMilliseconds()
+  );
+});
+export const setS = curry((seconds: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    seconds,
+    date.getMilliseconds()
+  );
+});
+export const setMS = curry((milliseconds: number, date: Date) => {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds(),
+    milliseconds
+  );
+});
